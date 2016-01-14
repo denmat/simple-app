c6533e24b62f4e82defad5b514c68bd7f61f6f14
DenMat <dennis.matotek@envato.com>
2016-01-14 23:17:48 +1100

    remember to remove dead containers

--- scripts/test_container.sh
+++ scripts/test_container.sh
@@ -5,4 +5,5 @@ source scripts/common.sh
 
 docker run --name simple_app -d simpleapp_simple tail -f /dev/null && \
   docker exec simple_app ping -c 15 google.com && \
-  docker kill simple_app
+  docker kill simple_app && \
+  docker rm simple_app
