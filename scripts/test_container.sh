30a31cdf88ae6b3d168e6d27ac9edb5f9a3e1c02
DenMat <dennis.matotek@envato.com>
2016-01-14 23:09:01 +1100

    setting up containers to properly run tests.

--- scripts/test_container.sh
+++ scripts/test_container.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 source scripts/common.sh
+source scripts/common.sh
 
-docker run --name simple_app --rm simpleapp_simple
-
-docker exec simpleapp_simple ping -c 15 google.com
-
-docker kill simpleapp_simple
+docker run --name simple_app -d simpleapp_simple tail -f /dev/null && \
+  docker exec simple_app ping -c 15 google.com && \
+  docker kill simple_app
